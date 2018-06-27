3934d3fd90261dbe7a9c562043fe300fb6101f00
Giannis Afentoulis <gafentoulis@draxis.gr>
2018-06-27 13:27:53 +0300

    animation working

--- geojson.js
+++ geojson.js
@@ -1,4 +1,48 @@
+//THE MAIN GEOJSON FILE
 var geojson = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [
+                [0, 0.5],[0, 1]
+            ]
+        },
+        "direction": [3,5]
+    },{
+      "type": "Feature",
+      "geometry": {
+          "type": "LineString",
+          "coordinates": [
+              [0.5, 0.5],[0.5, 1]
+          ]
+      }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+        "type": "LineString",
+        "coordinates": [
+            [1, 0.8],[1, 1]
+        ]
+    }
+},
+{
+  "type": "Feature",
+  "geometry": {
+      "type": "LineString",
+      "coordinates": [
+          [1.5, 1.1],[1.5, 1]
+      ]
+  }
+},
+  ]
+};
+
+// TO KEEP TRUCK OF THE ANIMATION AND INITIALIZE THE VALUES
+var geojson1 = {
     "type": "FeatureCollection",
     "features": [
       {
@@ -9,6 +53,65 @@ var geojson = {
                 [0, 0.1],[0, 1]
             ]
         }
+    },{
+      "type": "Feature",
+      "geometry": {
+          "type": "LineString",
+          "coordinates": [
+              [0.5, 0.5],[0.5, 1]
+          ]
+      }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+        "type": "LineString",
+        "coordinates": [
+            [1, 0.8],[1, 1]
+        ]
     }
+},
+{
+  "type": "Feature",
+  "geometry": {
+      "type": "LineString",
+      "coordinates": [
+          [1.5, 1.1],[1.5, 1]
+      ]
+  }
+},
   ]
 };
+//GEOJSON POLYGON
+var geojsonPolygon = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.162109375,
+              46.70973594407157
+            ],
+            [
+              16.34765625,
+              42.293564192170095
+            ],
+            [
+              18.193359375,
+              46.49839225859763
+            ],
+            [
+              11.162109375,
+              46.70973594407157
+            ]
+          ]
+        ]
+      }
+    }
+  ]
+}
--- index.js
+++ index.js
@@ -16,30 +16,44 @@ map.on('load', function() {
       'data': geojson
     },
     'paint': {
-      'line-color':'red',
+      'line-color':'#d3d3d3',
       'line-width': 1,
-      'line-opacity': .8
+      'line-opacity': .8,
+      'line-opacity-transition': {duration: 10000}
+
     }
   })
 
-  var x1 = geojson.features[0].geometry.coordinates[0][0];
-  var y1 = geojson.features[0].geometry.coordinates[0][1];
-  var x2 = geojson.features[0].geometry.coordinates[1][0];
-  var y2 = geojson.features[0].geometry.coordinates[1][1];
-  test();
-  function test() {
-    console.log()
-    if (geojson.features[0].geometry.coordinates[1][1] < 3) {
-      geojson.features[0].geometry.coordinates[0][0] += 0.004;
-      geojson.features[0].geometry.coordinates[0][1] += 0.005;
-      geojson.features[0].geometry.coordinates[1][0] += 0.005;
-      geojson.features[0].geometry.coordinates[1][1] += 0.005;
-    } else {
-      geojson.features[0].geometry.coordinates[0][0] = x1;
-      geojson.features[0].geometry.coordinates[0][1] = y1;
-      geojson.features[0].geometry.coordinates[1][0] = x2;
-      geojson.features[0].geometry.coordinates[1][1] = y2;
+  map.addLayer({
+    'id': 'polygonTest',
+    'type': 'fill',
+    'source': {
+      'type': 'geojson',
+      'data': geojsonPolygon
+    },
+    'paint': {
+      'fill-color':'#d3d3d3',
+      'fill-opacity':.3
     }
+  })
+
+  test();
+  function test(timestamp) {
+    for (i = 0; i < geojson.features.length; i++) {
+      if (geojson.features[i].geometry.coordinates[1][1] < geojson1.features[i].geometry.coordinates[1][1] + 2) {
+        geojson.features[i].geometry.coordinates[0][0] += 0.004;
+        geojson.features[i].geometry.coordinates[0][1] += Math.sin(0.010);
+        geojson.features[i].geometry.coordinates[1][0] += 0.005;
+        geojson.features[i].geometry.coordinates[1][1] += 0.005;
+        map.setPaintProperty('line-animation', 'line-opacity', 0);
+      } else {
+        geojson.features[i].geometry.coordinates[0][0] = geojson1.features[i].geometry.coordinates[0][1];
+        geojson.features[i].geometry.coordinates[0][1] = geojson1.features[i].geometry.coordinates[0][1];
+        geojson.features[i].geometry.coordinates[1][0] = geojson1.features[i].geometry.coordinates[1][0];
+        geojson.features[i].geometry.coordinates[1][1] = geojson1.features[i].geometry.coordinates[1][1];
+        map.setPaintProperty('line-animation', 'line-opacity', 1);
+      }
+    };
     map.getSource('line-animation').setData(geojson);
     requestAnimationFrame(test);
   }
